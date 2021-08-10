2acae0e8b97b3a035b3e8ee422b196007553ce2f
阿敗塗一地 <cub3r.ezra.yu@gmail.com>
2021-08-11 03:11:52 +0800

    footer html

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     $('.carousel').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
     });
   });
